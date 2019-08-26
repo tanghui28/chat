2c9c7cc922b8132ee732c9dff4b646e32e088f61
tanghui28 <425475513@qq.com>
2019-08-26 17:53:56 +0800

    sql

---
+++ chat.sql
@@ -0,0 +1,29 @@
+SET NAMES utf8;
+DROP DATABASE IF  EXISTS chat;
+CREATE DATABASE CHAT CHARSET=UTF8;
+#用户表
+CREATE TABLE user(
+  user_id INT PRIMARY KEY AUTO_INCREMENT,
+  uname VARCHAR(100) UNIQUE NOT NULL,
+  gender BOOL NOT NULL DEFAULT 1,
+  phone CHAR(11) NOT NULL,
+  avatar VARCHAR NOT NULL DEFAULT 'male.png',
+  createAt BIGINT NOT NULL,
+  UPDATEAT BIGINT NOT NULL
+)
+#好友关系表
+CREATE  TABLE user_friend(
+  user_id INT NOT NULL,
+  friend_id INT NOT NULL,
+  createAt BIGINT NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES user(user_id),
+  FOREIGN KEY(friend_id) REFERENCES user(user_id)
+)
+#用户动态表
+CREATE TABLE user_news(
+  nid INT PRIMARY KEY AUTO_INCREMENT,
+  news_text VARCHAR(255) NOT NULL,
+  news_img VARCHAR(255) NOT NULL DEFAULT "",
+  user_id INT NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES user(user_id)
+)
