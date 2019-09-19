da28e1cb5447d710e97d0c190712791a46873782
tanghui28 <425475513@qq.com>
2019-09-19 22:04:40 +0800

    s

---
+++ api/model/user_friend.js
@@ -0,0 +1,30 @@
+// 数据库 orm 映射模块
+const Sequelize = require('sequelize');
+
+const config = require('../config');
+let sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  dialect: 'mysql',
+  pool: {
+    max: 10,
+    min: 0,
+    idle: 30000
+  }
+})
+// 定义user模型
+let userFriendModel = sequelize.define('user_friend',
+  {
+    user_id: Sequelize.INTEGER,
+    friend_id:Sequelize.INTEGER,
+    friend_remark:Sequelize.STRING(8),
+    friend_message:Sequelize.STRING(9999),
+    createdAt: Sequelize.BIGINT,
+  },
+  {
+  timestamps: false,
+  tableName: "user_friend"
+  }
+);
+
+module.exports = userModel;
+
