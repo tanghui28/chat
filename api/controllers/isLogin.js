771522e571f13ee8669f25e6a9a530a066109bdd
tanghui28 <425475513@qq.com>
2019-09-24 17:53:13 +0800

    s

--- api/controllers/isLogin.js
+++ api/controllers/isLogin.js
@@ -8,13 +8,14 @@ const jwt = require('jsonwebtoken');
 //引入 user model
 const userModel = require('../model/user');
 
+const IP = require('../getIP');
 
 let fn_isLogin = async(ctx,next)=>{
 
   let token = ctx.request.body.token;
   // 秘钥
   let secret = "tanghui";
-  jwt.verify(token,secret,(err,decode)=>{
+  await jwt.verify(token,secret,async(err,decode)=>{
 
     if(err){  //时间过期或者伪造token
       // console.log(err)
@@ -27,12 +28,20 @@ let fn_isLogin = async(ctx,next)=>{
       })
     }else{
       // console.log(decode)
-
+      let userInfo = await userModel.findAll({
+        attributes: ['avatar','forbidden','gender','phone','uname','user_id'],
+        where: {
+          user_id:decode.user_id
+        },
+        raw: true
+      })
+      userInfo[0].token = token;
+      userInfo[0].avatar = "http://" + IP + ':3000/images/' + userInfo[0].avatar;
       ctx.response.body = JSON.stringify({
         title: '验证token',
         success: true,
         info: '用户已登录',
-        data: null
+        data: userInfo[0]
       })
     }
 
