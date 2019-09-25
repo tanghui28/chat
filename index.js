7b3cfbbfeb5bc55556319e550f94987a187b97d7
tanghui28 <425475513@qq.com>
2019-09-25 17:57:47 +0800

    下班

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+const querystring = require('querystring');
 // 文件读取模块
 const fs = require('fs');
 // koa
@@ -73,35 +75,73 @@ addControllers(router);
 // 创建koa服务
 const app = new koa();
 
-const server = require('http').Server(app.callback());
-// 启动websocket服务
-const io = require('socket.io')(server);
-io.on('connection', socket => {
-  console.log(socket)
-  // setTimeout(() => {
-  //   socket.emit('message', [1,2,3])
-  // }, 3000);
-  setInterval(() => {
-    socket.emit('message', [1,2,3])
-  }, 5000);
+// const server = require('http').Server(app.callback());
 
-  socket.on('connect',data=>{
-    console.log(data)
-  })
+// const userModel = require('./api/model/user');
+// // 启动websocket服务
+// const io = require('socket.io')(server);
+// io.on('connection', async socket => {
+//   console.log(socket)
+//   const secret = 'tanghui';
+//   await jwt.verify(socket.handshake.query.token, secret, async(err,decode) => { 
+
+//     if (!err) { 
+
+//       console.log(decode.user_id);
+//       await userModel.update(
+//         {
+//           online: 1,
+//           socketId: socket.id
+//         },
+//         {
+//           where: {
+//             'user_id': 1
+//           }
+//         }
+//       ).then(ok => { 
+//         console.log(ok)
+//       }).catch(err => { 
+//         console.log(err)
+//       })
+
+//       let res = await userModel.findOne({
+//         attributes: ['online', 'socketId'],
+//         where: {
+//           user_id: decode.user_id
+//         },
+//         raw:true
+//       })
+//       console.log(res);
 
 
-  socket.on('top', data => { 
-    console.log(data)
-  })
+//     }
+
+//   })
+
+//   setInterval(() => {
+//     socket.emit('message', [1,2,3])
+//   }, 15000);
+
+//   socket.on('connect',data=>{
+//     console.log(data)
+//   })
 
 
-  socket.on('disconnect', data => { 
-    console.log('用户断开连接'+data)
-  })
+//   socket.on('top', data => { 
+//     console.log(data)
+//   })
 
-})
 
-server.listen(3000)
+//   socket.on('disconnect', data => { 
+//     console.log('用户断开连接'+data)
+//   })
+
+// })
+
+// server.listen(3000)
+
+
+
 
 
 
@@ -109,7 +149,46 @@ app.use(static(__dirname+'/public'))
 app.use(bodyParser());                //必须在router之前注册到app对象上
 app.use(router.routes());
 app.use(cors());
-// app.listen(3000);
+let sp = app.listen(3000);
+
+
+let list = [];          //在线客户端列表
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({
+  server: sp
+})
+wss.on('connection', (ws, req) => {
+  console.log('已连接');
+  list.push(ws);
+  console.log(list);
+  let params = querystring.parse( req.url.slice(2) );
+  // { 
+  //   user_id:1
+  // }
+
+  // ws.onmessage = (msg) => { 
+   
+  // }
+  ws.on('message', (data) => { 
+    
+    if (data === "ping") { 
+      console.log(data);
+      ws.send('ping');
+      return;
+    }
+    data = JSON.parse(data);
+    console.log(data);
+    // {from:'Client A', to:'Client B', body: 'hello'}
+
+   
+  })
+
+  ws.on('close', () => { 
+    console.log('用户断开连接')
+    _.pull(list, ws);
+  })
+
+})
 
 
 
